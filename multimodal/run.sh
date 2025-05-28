bb8128ded0f9112fd6c0c17a54a42d4091619759
Tonge-Shim <tonge20@naver.com>
2025-05-28 15:36:48 +0900

    update

---
+++ multimodal/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+unset LD_LIBRARY_PATH
+# 환경 변수 설정
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+export TOKENIZERS_PARALLELISM=false
+export PYTHONPATH="$(dirname $(pwd))":"$(pwd)"
+
+# 학습 실행
+python train.py
